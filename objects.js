27d93c45ca6ca7623e6f86f8f619010c6f6484c1
Brendan-funk <brendanzfunk@gmail.com>
2022-01-15 03:03:13 +0000

    added object, object-keys, and object-properties

---
+++ objects.js
@@ -0,0 +1,10 @@
+const pizza = {  
+  toppings: ['cheese', 'sauce', 'pepperoni'],  
+  crust: 'deep dish',  
+  serves: 2  
+} 
+console.log(pizza);
+const food = {
+  types: "only pizza"
+}
+console.log(food);
